0ef4aa94148350394f4205c8473d9fc37ee695c4
Vuyisanani Meteni <vuyisanani@Vuyisananis-MacBook-Pro.local>
2019-05-22 13:47:31 +0200

    Created a lit of heroes componet

---
+++ tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Vuyisanani' },
+    { id: 12, name: 'Lubabalo' },
+    { id: 13, name: 'Thambo' },
+    { id: 14, name: 'Lindelwa' },
+    { id: 15, name: 'Mahleza' },
+    { id: 16, name: 'Lizwi' },
+    { id: 17, name: 'Siphamandla' },
+    { id: 18, name: 'Ayanda' },
+    { id: 19, name: 'Buyisile' },
+    { id: 20, name: 'Meteni' }
+  ];
